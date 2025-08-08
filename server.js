1f42e776d474c3737d506171cc914329f2ec43e8
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-08-08 17:27:17 +0200

    post method completed

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express')
+const express = require("express");
+const fs = require("fs");
 const app = express();
- 
+app.use(express.json());
 
 // YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+app.post("/blogs", function (req, res) {
+  fs.writeFileSync(req.body.title, req.body.content);
+  res.end("ok");
+});
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000);
