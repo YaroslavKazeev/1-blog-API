67d1824547c2e421695d2170774648cb692292ff
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-08-09 20:10:09 +0200

    put method fixed, added delete and read methods

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.put("/posts/:title", (req, res) => {
   const { content } = req.body;
   // How to get the title and content from the request?
   // What if the request does not have a title and/or content?
-  if (!title || !content) {
+  if (!content) {
     res.status(400);
-    res.json({ error: "Both title and content are required" });
+    res.json({ error: "Content is required" });
   } else if (fs.existsSync(title)) {
     try {
       fs.writeFileSync(title, content);
@@ -28,6 +28,42 @@ app.put("/posts/:title", (req, res) => {
     } catch (error) {
       res.status(500);
       res.send({ error: "Error updating the post" });
+    }
+  } else {
+    res.statusCode = 404;
+    res.send({ error: "Post not found" });
+  }
+});
+
+app.delete("/blogs/:title", (req, res) => {
+  const { title } = req.params;
+  // How to get the title from the url parameters?
+  if (fs.existsSync(title)) {
+    try {
+      res.statusCode = 200;
+      res.setHeader("Content-type", "text-plain");
+      res.end("ok");
+    } catch (error) {
+      res.status(500);
+      res.send({ error: "Error deleting the post" });
+    }
+  } else {
+    res.statusCode = 404;
+    res.send({ error: "Post not found" });
+  }
+});
+
+app.get("/blogs/:title", (req, res) => {
+  const { title } = req.params;
+  if (fs.existsSync(title)) {
+    try {
+      const post = fs.readFileSync(title);
+      res.statusCode = 200;
+      res.setHeader("Content-type", "application/json");
+      res.end(post);
+    } catch (error) {
+      res.status(500);
+      res.send({ error: "Error retrieving the post" });
     }
   } else {
     res.statusCode = 404;
